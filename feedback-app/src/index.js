b2b861d2a358fe12c5263fc180112accfe7797f2
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-07-29 23:11:49 -0500

    added more starter code

--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -1,5 +1,6 @@
+//Index.js is the entry point of the application.
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
