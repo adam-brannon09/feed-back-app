1d3f61839e5c82929666eb1d582c2183db5070c8
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-09-05 12:59:52 -0500

    added cards, light/dark themes, implemented style based on conditions

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,19 @@
-import card from "./shared/card"
+import Card from "./shared/Card"
+import Proptypes from "prop-types"
 
 
 //item are passed in from FeedbackList.jsx. 
 function FeedbackItem({ item }) {
     return (
-        <div className='card'>
+        <Card>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}</div>
-        </div>
+        </Card>
     )
+}
+
+FeedbackItem.propTypes = {
+    item: Proptypes.object.isRequired,
 }
 
 export default FeedbackItem
--- src/components/shared/card.jsx
+++ src/components/shared/card.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function card() {
+function Card({ children, reverse }) {
     return (
-        <div>card</div>
+        // Style will change depending on reverse prop being true or false. basically in FeedbackItem.jsx, we pass in reverse={false} and reverse={true} to change the style of the card component. This is by using the ternary operator in the style attribute, on lines 8 and 9.
+        <div className='card'
+            style={{
+                backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : "#fff",
+                color: reverse ? "#fff" : "#000"
+            }}
+        >{children}</div>
     )
 }
 
-export default card
+Card.defaultProps = {
+    reverse: true,
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    reverse: PropTypes.bool,
+}
+
+
+export default Card
