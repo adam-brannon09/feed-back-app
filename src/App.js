4720a6117d18897ede985686cc95220b8608589c
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-09-06 00:18:59 -0500

    added uuid package. added form submit functionality to feedback form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import React from 'react';
 import './App.css';
@@ -10,6 +11,11 @@ import FeedbackForm from './components/FeedbackForm';
 function App() {
   //Feedback is the prop passed from app.js that refers to the array of objects in FeedbackData.js.
   const [feedback, setFeedback] = useState(FeedbackData);
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  }
+
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this feedback?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
@@ -19,7 +25,7 @@ function App() {
     <>
       <Header />
       <div className="App">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         {/* feedback is from the state that is referred to on line 10 */}
         <FeedbackList feedback={feedback}
