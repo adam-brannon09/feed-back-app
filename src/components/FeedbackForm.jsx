01fdfa21e64ce879181fd7f2ff248eb213fe549e
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-09-06 00:32:46 -0500

    fixed issue where text input wouldnt reset to placeholder after submitting a feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -22,6 +22,7 @@ function FeedbackForm({ handleAdd }) {
             setMessage(null)
         }
         setText(e.target.value)
+
     }
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -30,9 +31,11 @@ function FeedbackForm({ handleAdd }) {
                 text,
                 rating,
             }
+
             handleAdd(newFeedback)
             setText('')
         }
+
     }
 
     return (
@@ -41,7 +44,7 @@ function FeedbackForm({ handleAdd }) {
                 <h2>How would you rate your service with us?</h2>
                 <RatingSelect select={(rating) => setRating(rating)} />
                 <div className="input-group">
-                    <input onChange={handleTextChange} type="text" placeholder="Write a review" />
+                    <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
                     <Button type='submit' isDisabled={btnDisabled}>Send</Button>
                 </div>
                 {message && <div className="message">{message}</div>}
