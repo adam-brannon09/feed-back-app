5a2cfe8f20b5d65f0fb717f96a82ac78d4ca59c8
adam-brannon09 <adam.brannon09@gmail.com>
2023-09-19 11:54:33 -0500

    added context folder and file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
 import AboutIconLink from './components/AboutIconLink';
 
 function App() {
@@ -26,28 +27,29 @@ function App() {
   }
   return (
 
-    // The Router component is the parent component that wraps all the other components that will be using the react-router-dom library. The Router component is imported from react-router-dom.
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          The Routes component is the parent component that wraps all the Route components. The Routes component is imported from react-router-dom.
-          <Route exact path='/' element={
-            // Since FeedbackForm, FeedbackStats and FeedbackList are different components, they need to be wrapped in a parent element, in this case, a Fragment. Without the fragment an error will be thrown.
-            <>
-              <FeedbackForm handleAdd={addFeedback} />
-              <FeedbackStats feedback={feedback} />
-              {/* feedback is from the state that is referred to on line 10 */}
-              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </>
-          }>
-          </Route>
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
-
+    <FeedbackProvider>
+      {/* The Router component is the parent component that wraps all the other components that will be using the react-router-dom library. The Router component is imported from react-router-dom. */}
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            The Routes component is the parent component that wraps all the Route components. The Routes component is imported from react-router-dom.
+            <Route exact path='/' element={
+              // Since FeedbackForm, FeedbackStats and FeedbackList are different components, they need to be wrapped in a parent element, in this case, a Fragment. Without the fragment an error will be thrown.
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                {/* feedback is from the state that is referred to on line 10 */}
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }>
+            </Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
