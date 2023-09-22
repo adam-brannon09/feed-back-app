3af8fef77c9ebec52515fc92a5e835e21d0beec7
adam-brannon09 <adam.brannon09@gmail.com>
2023-09-22 13:49:20 -0500

    added spinner component for loading screen

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 
 function FeedbackList() {
-    const { feedback } = useContext(FeedbackContext);
+    const { feedback, isLoading } = useContext(FeedbackContext);
     //if there is no feedback or the feedback array is empty, return a paragraph that says 'No feedback yet'
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No feedback yet</p>
     }
     //map through the feedback array and return a FeedbackItem component for each item in the array. Item is the assigned parameter that refers to each item in the array. Feedback is the prop passed from app.js that refers to the array of objects in FeedbackData.js. 
-    return (
+    return isLoading ? (<Spinner />) : (
         <div className='feedback-list'>
             {feedback.map((item) => (
                 <FeedbackItem
