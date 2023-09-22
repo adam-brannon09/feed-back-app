695902b419ed174d5ec0008047992300607036aa
adam-brannon09 <adam.brannon09@gmail.com>
2023-09-22 14:29:32 -0500

    added update functionality with db

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -47,8 +47,17 @@ export const FeedbackProvider = ({ children }) => {
         }
     }
 
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`,
+            {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(updItem),
+            })
+
+        const data = await response.json()
+
+        setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...data } : item)))
 
 
     }
