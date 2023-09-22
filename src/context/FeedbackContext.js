b1e242009bcc7cac878fe0d3a15a7cbffc059f6e
adam-brannon09 <adam.brannon09@gmail.com>
2023-09-22 14:04:42 -0500

    add comment functionality working

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
+
 
 const FeedbackContext = createContext();
 
@@ -20,15 +20,24 @@ export const FeedbackProvider = ({ children }) => {
 
     //Fetch Feedback
     const fetchFeedback = async () => {
-        const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+        const response = await fetch('/feedback?_sort=id&_order=desc')
         const data = await response.json()
         setFeedback(data)
         setIsLoading(false)
     }
     //add new feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+
+        setFeedback([data, ...feedback]);
     };
     //delete feedback
     const deleteFeedback = (id) => {
